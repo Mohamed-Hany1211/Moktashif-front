81cf737c7e0599362fe1318d1e5ad76966c44f7a
Mohamed-Hany1211 <m.searg1905@gmail.com>
2025-03-11 01:10:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import Home from "./Components/Home/Home";
+import NotFound from "./Components/NotFound/NotFound";
+import Results from "./Components/Results/Results";
+import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
+import UserProfile from "./Components/UserProfile/UserProfile";
+
+let routers = createBrowserRouter([
+  {
+    path: '', element: <Layout />, children: [
+      { index: true, element: <Home /> },
+      { path: 'results', element: <Results /> },
+      {
+        path: 'signin', element: <SignIn />, 
+        // children: [
+        //   { path: 'signup', element: <SignUp /> }
+        // ]
+      },
+      { path: 'profile', element: <UserProfile /> },
+      { path: '*', element: <NotFound /> }
+    ]
+  }
+]);
+
+function App() {
+  return <RouterProvider router={routers}></RouterProvider>
+}
+
+export default App
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='bg-gray-800 p-2 text-white text-center fixed-bottom'>
+            <h3>Footer</h3>
+        </footer>
+    )
+}
